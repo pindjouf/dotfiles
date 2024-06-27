6a6640c1cd9c6870779a6ba54f14262786a44505
pindjouf <esaubukasa@protonmail.com>
2024-06-27 10:00:43 +0200

    idk bruh

--- hyprlandd/nvim/init.lua
+++ hyprlandd/nvim/init.lua
@@ -228,7 +228,7 @@ vim.opt.rtp:prepend(lazypath)
 --
 --  To update plugins you can run
 --    :Lazy update
---
+--  All the plugs!
 -- NOTE: Here is where you install your plugins.
 require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
