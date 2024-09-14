89af2bad37b8114a9e7049307dc88374584e0488
pindjouf <esaubukasa@protonmail.com>
2024-09-14 16:03:16 +0200

    finish nixos script

--- hyprlandd/nixos/nix_installer.sh
+++ hyprlandd/nixos/nix_installer.sh
@@ -38,63 +38,68 @@ function diskDetector() {
     confirm "Are you sure you want to use $disk? This will erase all data on the disk." || exit 1
 }
 
-# Main
-printf "Welcome to the NixOS installer script\n"
+function main() {
 
-diskDetector
+    # Main
+    printf "Welcome to the NixOS installer script\n"
 
-# Detect boot mode
-if [[ -d "/sys/firmware/efi" ]]; then
-    boot_mode="uefi"
-else
-    boot_mode="bios"
-    printf "This script doesn't currently support legacy bios sorry :(\n"
-    exit 0
-fi
+    diskDetector
 
-printf "\n"
-printf "Detected boot mode: $boot_mode\n"
+    # Detect boot mode
+    if [[ -d "/sys/firmware/efi" ]]; then
+        boot_mode="uefi"
+    else
+        boot_mode="bios"
+        printf "This script doesn't currently support legacy bios sorry :(\n"
+        exit 0
+    fi
 
-# UEFI (GPT) partition scheme
-printf "\n"
-printf "Partitioning your nix system...\n"
-printf "\n"
-parted "$disk" -- mklabel gpt
-parted "$disk" -- mkpart root ext4 512MB -8GB
-parted "$disk" -- mkpart swap linux-swap -${swap_size}MB 100%
-parted "$disk" -- mkpart ESP fat32 1MB 512MB
-parted "$disk" -- set 3 esp on
+    printf "\n"
+    printf "Detected boot mode: $boot_mode\n"
 
-# Formatting
-printf "\n"
-printf "Formatting your nix system...\n"
-printf "\n"
-mkfs.ext4 -L nixos "$disk"1
-mkswap -L swap "$disk"2
-mkfs.fat -F 32 -n boot "$disk"3
+    # UEFI (GPT) partition scheme
+    printf "\n"
+    printf "Partitioning your nix system...\n"
+    printf "\n"
+    parted "$disk" -- mklabel gpt
+    parted "$disk" -- mkpart root ext4 512MB -8GB
+    parted "$disk" -- mkpart swap linux-swap -${swap_size}MB 100%
+    parted "$disk" -- mkpart ESP fat32 1MB 512MB
+    parted "$disk" -- set 3 esp on
 
-# Install System
-printf "\n"
-printf "Installing the base system...\n"
-printf "\n"
-mount /dev/disk/by-label/nixos /mnt
-mkdir -p /mnt/boot
-mount -o umask=077 /dev/disk/by-label/boot /mnt/boot
-swapon "$disk"2
-nixos-generate-config --root /mnt
+    # Formatting
+    printf "\n"
+    printf "Formatting your nix system...\n"
+    printf "\n"
+    mkfs.ext4 -L nixos "$disk"1
+    mkswap -L swap "$disk"2
+    mkfs.fat -F 32 -n boot "$disk"3
 
-# Get My Config From GitHub
-printf "\n"
-printf "Getting your configuration.nix file from GitHub...\n"
-printf "\n"
-nix-env -iA nixpkgs.git
-git clone https://github.com/pindjouf/dotfiles.git
-mv dotfiles/hyprlandd/nixos/configuration.nix /mnt/etc/nixos/configuration.nix
-rm -r dotfiles
-nixos-install
+    # Install System
+    printf "\n"
+    printf "Installing the base system...\n"
+    printf "\n"
+    mount /dev/disk/by-label/nixos /mnt
+    mkdir -p /mnt/boot
+    mount -o umask=077 /dev/disk/by-label/boot /mnt/boot
+    swapon "$disk"2
+    nixos-generate-config --root /mnt
 
-printf "\n"
-printf "NixOS installation complete!\n"
-printf "\n"
+    # Get My Config From GitHub
+    printf "\n"
+    printf "Getting your configuration.nix file from GitHub...\n"
+    printf "\n"
+    nix-env -iA nixpkgs.git
+    git clone https://github.com/pindjouf/dotfiles.git
+    mv dotfiles/hyprlandd/nixos/configuration.nix /mnt/etc/nixos/configuration.nix
+    rm -r dotfiles
+    nixos-install
 
-confirm "Do you want to reboot now?" && reboot || printf "You can reboot manually when ready.\n"
+    printf "\n"
+    printf "NixOS installation complete!\n"
+    printf "\n"
+
+    confirm "Do you want to reboot now?" && reboot || printf "You can reboot manually when ready.\n"
+}
+
+main
