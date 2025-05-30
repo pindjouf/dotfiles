2e3349711fbfb20840fe56b976a35d40aed3d5d6
pindjouf <esaubukasa@protonmail.com>
2025-05-30 07:27:19 +0200

    hehe

--- hyprlandd/waybar/scripts/bitcoin.sh
+++ hyprlandd/waybar/scripts/bitcoin.sh
@@ -4,7 +4,7 @@ PRICE=$(curl -s https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT | jq 
 
 if [ -n "$PRICE" ]; then
     FORMATTED_PRICE=$(printf "%.2f" $PRICE)
-    echo "{\"text\": \"   $FORMATTED_PRICE\", \"class\": \"bitcoin\"}"
+    echo "{\"text\": \"  $FORMATTED_PRICE\", \"class\": \"bitcoin\"}"
 else
     echo "{\"text\": \"Error fetching price\", \"class\": \"error\"}"
 fi
